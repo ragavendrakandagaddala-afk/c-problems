ea3a13e1608dfffcaa362e9486b6ed8d264efc3e
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2025-12-28 21:40:44 +0530

    Add files via upload

---
+++ 28-12-2025/28-12-2025.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int main() {
+    int a = 10, b = 25, c = 15;
+    if (a >= b) {
+        if (a >= c) {
+            printf("%d is largest",a);
+        } else {
+            printf("%d is largest",c);
+        }
+    } else {
+        if (b >= c) {
+            printf("%d is largest",b);
+        } else {
+            printf("%d is largest",c);
+        }
+    }
+    return 0;
+}
