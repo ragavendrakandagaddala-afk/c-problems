b217a7e1e7a723b6e309572011ffe0b89305f9ab
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-01-02 21:02:20 +0530

    Add files via upload

---
+++ 02-01-2026/02-01-2026-03.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,i,arr[10];
+    int max;
+    printf("Entyer the value:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    for(i=1;i<n;i++){
+    if(arr[i]>max){
+        max=arr[i];}
+    }
+    printf("%d",max);
+
+return 0;
+}
