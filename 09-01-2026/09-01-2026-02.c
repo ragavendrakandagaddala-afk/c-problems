fbb3daea97e138596b90c6be978cc59313c12a74
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-01-09 21:16:40 +0530

    Add files via upload

---
+++ 09-01-2026/09-01-2026-02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+
+    printf("Lowercase string: %s", str);
+
+    return 0;
+}
