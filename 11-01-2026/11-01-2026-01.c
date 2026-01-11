d86b58fb0e01e963fe586eded036a62a2859c057
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-01-11 20:42:02 +0530

    Add files via upload

---
+++ 11-01-2026/11-01-2026-01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, gcd;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    while (b != 0) {
+        gcd = b;
+        b = a % b;
+        a = gcd;
+    }
+    printf("GCD = %d", a);
+    return 0;
+}
