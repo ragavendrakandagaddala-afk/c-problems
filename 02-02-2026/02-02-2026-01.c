0786c9310a8c095af61ae82d9c4cfd284c11956d
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-02-02 22:27:11 +0530

    Add files via upload

---
+++ 02-02-2026/02-02-2026-01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void checkEvenOdd(int n) {
+    if (n % 2 == 0)
+        printf("%d is Even\n", n);
+    else
+        printf("%d is Odd\n", n);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    checkEvenOdd(num);
+    return 0;
+}
