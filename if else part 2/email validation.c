0db20bf517225db337e6f25b4d766fbe200b4053
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-02-09 22:23:39 +0530

    Create email validation.c

---
+++ if else part 2/email validation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d",&n);
+
+    if (n > 5) {
+        printf("valid Email");
+    } else {
+        printf("Invalid Email");
+
+    }
+
+
+    return 0;
+}
