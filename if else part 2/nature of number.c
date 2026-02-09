98133a2670664cc3c39a7b8aefbe4cf3ace4d83a
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-02-09 22:34:10 +0530

    Create nature of number.c

---
+++ if else part 2/nature of number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d",&n);
+
+    if (n > 0) {
+        printf("positive");
+    } else if (n == 0) {
+        printf("Neutral");
+    } else {
+        printf("Negative");
+    }
+
+
+    return 0;
+}
