52ecee9249b7702d0947e672ba41396062cf27c5
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-02-09 22:43:38 +0530

    Create salary bonus.c

---
+++ if else part 2/salary bonus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int salary;
+
+    scanf("%d",&salary);
+
+    if (salary > 30000) {
+        printf("Bonus Eligible");
+    } else {
+        printf("Not Eligible");
+    }
+
+    return 0;
+}
