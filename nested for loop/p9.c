0de65a07ef059ecce7289c254ed852853dcf0fc7
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-02-27 13:45:25 +0530

    Add nested loops for printing pattern
    Implement nested loops to print a pattern based on user input.

---
+++ nested for loop/p9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+     int n;
+     scanf("%d",&n);
+     for(int i=0;i<=n;i++){
+          for(int j=1;j<i+1;j++){
+               printf("* ");
+          }
+             printf("\n");
+     }
+     for(int i=n;i>0;i--){
+          for(int j=1;j<=i-1;j++){
+               printf("* ");
+          }
+             printf("\n");
+     }
+     return 0;
+}
