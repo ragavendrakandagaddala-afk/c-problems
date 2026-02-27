4229b2ec5b2c16c8a8057e008f7ea0d6a9698c42
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-02-27 13:46:07 +0530

    Add nested for loop to print alternating 1s and 0s

---
+++ nested for loop/p10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+     int n,num;
+     scanf("%d",&n);
+     for(int i=1;i<=n;i++){
+          if(i%2==1){
+               num=1;
+          }else
+            num=0;
+          for(int j=1;j<=i;j++){
+               printf("%d",num);
+               if(num==1){
+                    num=0;
+               }
+               else
+               num=1;
+          }
+             printf("\n");
+     }
+     return 0;
+}
