2e38a4ad2397d60089a6c4215d04cba3f4e4deb4
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-03-02 23:04:20 +0530

    Add nested for loop pattern printing in p19.c
    Implement a pattern printing program using nested loops.

---
+++ nested for loop/p19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, n = 5;
+    for(i = n; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+            printf("*");
+        for(j = 1; j <= 2*(n-i); j++)
+            printf(" ");
+        for(j = 1; j <= i; j++)
+            printf("*");
+        printf("\n");
+    }
+    for(i = 2; i <= n; i++)
+    {
+        for(j = 1; j <= i; j++)
+            printf("*");
+        for(j = 1; j <= 2*(n-i); j++)
+            printf(" ");
+        for(j = 1; j <= i; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
