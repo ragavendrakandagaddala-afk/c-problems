497c413872814b07e4f94ab45a0b0be95ef5ca63
ragavendrakandagaddala-afk <ragavendrakandagaddala@gmail.com>
2026-04-04 22:34:58 +0530

    Add Fibonacci sequence generator in C
    Implement Fibonacci sequence calculation using a for loop.

---
+++ for loops/Foor loop febonnaic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int first=0,second=1,next;
+    if(n>1)
+    printf(" %d",first);
+    if(n>2)
+    printf(" %d",second);
+    for(int i=3;i<=n;i++){
+        next=first+second;
+        printf(" %d",next);
+        first=second;
+        second=next;
+    }
+    return 0;
+}
